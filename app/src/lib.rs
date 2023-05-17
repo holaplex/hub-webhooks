1b184d80777a14c3b38a4cb43d7980b4d11b2572
mpw <x@mpw.sh>
2023-05-17 14:05:06 -0300

    remove async_closure from webhooks to use stable

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(async_closure)]
 #![deny(clippy::disallowed_methods, clippy::suspicious, clippy::style)]
 #![warn(clippy::pedantic, clippy::cargo)]
 #![allow(clippy::module_name_repetitions)]
